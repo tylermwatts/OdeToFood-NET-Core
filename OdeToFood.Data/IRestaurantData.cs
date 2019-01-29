49ae6f93fb9f1878431a6b8ddcfef1ead09cea85
Tyler <tylermwatts@gmail.com>
2019-01-29 10:07:28 -0600

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Tyler's Pizza", Location="Tennessee", Cuisine=CuisineType.Italian},
+                new Restaurant {Id = 2, Name = "Tyler's Burritos", Location="Tennessee", Cuisine=CuisineType.Mexican},
+                new Restaurant {Id = 3, Name = "Tyler's Tikka Masala", Location="Tennessee",Cuisine=CuisineType.Indian}
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+
+}
